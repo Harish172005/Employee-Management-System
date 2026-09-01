e8efc57122f288464a66b15042553010e0a975af
Harish172005 <harishkumar22it029@gmail.com>
2026-09-01 11:25:43 +0530

    Modify complex code into simpler

--- utilities/PasswordHasher.php
+++ utilities/PasswordHasher.php
@@ -1,26 +1,14 @@
 <?php
 
-class PasswordHasher {
-    
-    // Bcrypt algorithm configuration
-    const ALGORITHM = PASSWORD_BCRYPT;
-    const COST = 10;
-    
-    
-    public static function hash($password) {
-        return password_hash($password, self::ALGORITHM, ['cost' => self::COST]);
+class PasswordHasher
+{
+    public static function hash($password)
+    {
+        return password_hash($password, PASSWORD_DEFAULT);
     }
-    
-   
-    public static function verify($password, $hash) {
+
+    public static function verify($password, $hash)
+    {
         return password_verify($password, $hash);
     }
-    
-    
-    public static function needsRehash($hash) {
-        return password_needs_rehash($hash, self::ALGORITHM, ['cost' => self::COST]);
-    }
-    
-   
 }
-?>
